de15ca67fcdd39ae2cd4048751d96fed6c7a3768
Māris Lāčplēsis Zemgalis <zemgalis@users.noreply.github.com>
2022-05-23 00:10:52 +0100

    Add Firebase setup #4

---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyASSnfYQwVE6t4ucedEdwGVdz_IJ_dgxw8",
+  authDomain: "online-trading-platform.firebaseapp.com",
+  projectId: "online-trading-platform",
+  storageBucket: "online-trading-platform.appspot.com",
+  messagingSenderId: "838721999159",
+  appId: "1:838721999159:web:84c4018e3a06477ac20ab2",
+  measurementId: "G-TRH99731EB"
+};
